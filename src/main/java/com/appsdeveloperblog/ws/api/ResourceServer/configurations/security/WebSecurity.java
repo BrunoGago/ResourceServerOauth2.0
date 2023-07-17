526b95828a3fc19fbef4b544912edb8d59f0b449
Bruno Gago <bruno.gago@ab-inbev.com>
2023-07-17 14:57:32 -0300

    Method Level Security implemented

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/configurations/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/configurations/security/WebSecurity.java
@@ -1,11 +1,15 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.configurations.security;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+
+//@Secured
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)//enable the add secure anotation above the methods and classes
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
@@ -18,10 +22,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         //Configure Http Security
+        //Obs: User role can be defined in any of the methods above
         httpSecurity.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/status/check")
                 //.hasAuthority("SCOPE_profile")
                 .hasAnyRole("developer")
+                //.hasAnyAuthority("ROLE_developer")
+                //.hasAnyRole("developer", "user)
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer().jwt()
--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.appsdeveloperblog.ws.api.ResourceServer.model.UserResponse;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -11,6 +15,20 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status(){
         return "working...";
+    }
+
+    //@PreAuthorize("hasRole('developer')")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")//you can use both annotations
+    //@Secured("ROLE_developer")//Just users with the role provided can access this method (Note: The "ROLE_" must be used in this annotation)
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "Deleted user with id" + id + "and JWT Subject" + jwt.getSubject();//The subject is mostly equals to the ID
+    }
+
+    @PostAuthorize("returnObject.id == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserResponse getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return new UserResponse("Bruno", "Gago", "");
     }
 
 }
